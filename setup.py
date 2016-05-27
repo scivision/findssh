0fe84d43866cb6d914ec49c8932da38b00913c32
scienceopen <scienceopen@users.noreply.github.com>
2016-05-27 18:46:46 -0400

    autoreq

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+with open('README.rst','r') as f:
+    long_description = f.read()
+
+with open('requirements.txt','r') as f:
+    req = f.read().splitlines()
+
 setup(name='findssh',
       version='0.1',
 	    description='find servers with open ports without nmap',
@@ -8,6 +14,6 @@ setup(name='findssh',
 	    author='Michael Hirsch',
 	    url='https://github.com/scienceopen/findssh',
       dependency_links = [],
-	  install_requires=['six'],
+	  install_requires=[req],
       packages=[],
 	  )
