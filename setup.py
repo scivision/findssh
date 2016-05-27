9449ffad9c6eb3d9dd0f865e482465bb11ae1b4b
Michael Hirsch <scienceopen@users.noreply.github.com>
2016-05-27 18:45:06 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='findssh',
+      version='0.1',
+	    description='find servers with open ports without nmap',
+	    long_description=long_description,
+	    author='Michael Hirsch',
+	    url='https://github.com/scienceopen/findssh',
+      dependency_links = [],
+	  install_requires=['six'],
+      packages=[],
+	  )
