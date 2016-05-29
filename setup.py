56bc1cad942bde1bc35a2775f87d7fd275b7cc0d
michael <nobody@noreply.github.com>
2016-05-29 02:58:17 -0400

    new template

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 from setuptools import setup
+import subprocess
+
+try:
+    subprocess.run(['conda','install','--yes','--file','requirements.txt'])
+except Exception as e:
+    pass
+
 
 with open('README.rst','r') as f:
     long_description = f.read()
 
-with open('requirements.txt','r') as f:
-    req = f.read().splitlines()
 
 setup(name='findssh',
-      version='0.1',
-	    description='find servers with open ports without nmap',
-	    long_description=long_description,
-	    author='Michael Hirsch',
-	    url='https://github.com/scienceopen/findssh',
+      description='find servers with open ports without nmap',
+	  long_description=long_description,
+	  author='Michael Hirsch',
+	  url='https://github.com/scienceopen/findssh',
       dependency_links = [],
-	  install_requires=[req],
-      packages=[],
+	  install_requires = [],
 	  )
