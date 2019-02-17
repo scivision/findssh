ba12212a456c8e5581d7e4a5048094a06e2158e2
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-02-17 12:26:02 -0500

    update matlab api [skip ci]
    vers bump [skip ci]

--- findssh.m
+++ findssh.m
@@ -1,8 +1,8 @@
-function findssh(port, service, timeout, baseip)
+function hosts = findssh(port, service, timeout)
 %% FINDSSH  find SSH or other servers on an IPv4 subnet
 %
-% must first do one-time install in Python 3.6 from Terminal:
-%   pip install -e .
+% must first do one-time install in Python 3 from Terminal:
+%   python3 -m pip install -e .
 %
 % example (find all SSH servers on IPv4 subnet on Port 22):
 %   findssh()
@@ -12,17 +12,14 @@ assert(~verLessThan('matlab', '8.4'), 'Matlab >= R2014b required')
 if nargin < 1, port=22; end
 if nargin < 2, service = ''; end
 if nargin < 3, timeout = 0.1; end
-if nargin < 4, baseip = ''; end
-
 
 validateattributes(port, {'numeric'}, {'integer', 'nonnegative'})
 validateattributes(service, {'string', 'char'}, {'scalartext'})
 validateattributes(timeout, {'numeric'}, {'real', 'nonnegative'})
-validateattributes(baseip, {'string', 'char'}, {'scalartext'})
 
 % Matlab R2018b didn't like ThreadPoolExectutor
-servers = py.findssh.run(uint16(port), service, timeout, baseip, true);
+servers = py.findssh.main('', port, service, timeout);
 
 hosts = cellfun(@char, cell(servers), 'uniformoutput', false);
-disp(hosts)
+
 end
